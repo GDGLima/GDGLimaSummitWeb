712b1b1838081f48721bf420ff31f6c6ad5e99fa
Milton Josimar Yarleque Yovera <myarlequ@pe.ibm.com>
2016-10-07 18:55:41 -0500

    web version 1
    web version 1

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('gdglimaSummit2016', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngRoute', 'ngMaterial', 'toastr']);
-
-})();
